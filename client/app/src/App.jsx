ce316b1a4a89b2352e19484eff6447d0ad46183f
sviatoslavgit <semosvyatko@gmail.com>
2024-10-10 22:15:38 +0300

    added put and delete operations

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const [title, setTitle] = useState("");
   const [release_date, setReleaseDate] = useState(0);
 
+  const [newTitle, setNewTitle] = useState("")
+
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -34,10 +36,50 @@ function App() {
       body: JSON.stringify(bookData),
     });
     const data = await response.json();
-    console.log(data);
+    setBooks((prev) => [...prev, data]);
   }catch (err) {
     console.log(err);
   };}
+  
+  const updateTitle = async (pk, release_date) => {
+    const bookData = {
+      title: newTitle,
+      release_date,
+    };
+    try {
+    const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bookData),
+    });
+    const data = await response.json();
+    setBooks((prev) => prev.map((book) => {
+      if (book.id ===pk){
+        return data;
+      } else{
+        return book;
+      }
+    })
+  );
+  }catch (err) {
+    console.log(err);
+  };}
+
+  const deleteBook = async (pk) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "DELETE",
+      });
+
+      setBooks((prev) => prev.filter((book) => book.id !== pk));
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+
   return (
     <>
       <h1>Book Website</h1>
@@ -59,6 +101,13 @@ function App() {
         <div>
           <p>Title: {book.title}</p>
           <p>Release Year: {book.release_date}</p>
+          <input 
+            type='text' 
+            placeholder='New title'
+            onChange={(e) => setNewTitle(e.target.value)}
+          ></input>
+          <button onClick={() => updateTitle(book.id, book.release_date)}>Change title</button>
+          <button onClick={() => deleteBook(book.id)}>Delete</button>
         </div>
       ))}
     </>
@@ -66,4 +115,3 @@ function App() {
 }
 
 export default App
-
