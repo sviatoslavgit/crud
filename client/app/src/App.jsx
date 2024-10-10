6cb7fb0000e232e87ca70e1b592cdd2e1796a001
sviatoslavgit <semosvyatko@gmail.com>
2024-10-10 12:28:21 +0300

    added post to add new books to db

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [release_date, setReleaseDate] = useState(0);
 
   useEffect(() => {
     fetchBooks();
@@ -16,16 +18,42 @@ function App() {
     }catch(err){
       console.log(err);
     }
-  }
-
+  };
+  
+  const addBook = async () => {
+    const bookData = {
+      title,
+      release_date: release_date,
+    };
+    try {
+    const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bookData),
+    });
+    const data = await response.json();
+    console.log(data);
+  }catch (err) {
+    console.log(err);
+  };}
   return (
     <>
       <h1>Book Website</h1>
 
       <div>
-        <input type='text' placeholder='Book Title...'></input>
-        <input type='number' placeholder='Release Date'></input>
-        <button>Add book</button>
+        <input 
+        type='text' 
+        placeholder='Book Title...' 
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+        type='text' 
+        placeholder='Release Date'
+        onChange={(e) => setReleaseDate(e.target.value)}
+        />
+        <button onClick={addBook}>Add book</button>
       </div>
       {books.map((book) =>( 
         <div>
